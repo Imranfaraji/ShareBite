8a8982a02476b431aea19ebb1e173ae4b63ecaae
imran <imranfaraji880@gmail.com>
2025-08-10 16:40:58 +0600

    blog page design

---
+++ src/pages/Blogs/Blog.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Blog = ({ blog }) => {
+  const { date,  heading, image, _id } = blog;
+  return (
+    <div className="relative h-96 w-full">
+
+        <div className="w-full h-full bg-cover bg-center bg-no-repeat rounded-md" 
+        style={{backgroundImage:`url(${image})`}}></div>
+        <div className="absolute inset-0 bg-black opacity-70  rounded-md"></div>
+        <div className="absolute inset-0 flex flex-col items-start z-50 justify-end gap-2 p-4">
+
+            <h1 className="text-white text-sm font-bold ">{heading}</h1>
+            <p className="text-gray-200 text-xs font-semibold">{date}</p>
+            <Link className="btn btn-sm primary text-white border-none " to={`/blog/${_id}`}>Read more</Link>
+
+        </div>
+      
+    </div>
+  );
+};
+
+export default Blog;
--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,9 +1,37 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
+import Blog from './Blog';
 
 const Blogs = () => {
+    const {data:blogs=[],isLoading,isError}=useQuery({
+        queryKey:['blogs'],
+        queryFn: async ()=>{
+            const res= await axios.get('https://food-donet-server.vercel.app/blogs');
+            return res.data
+        }
+    })
+    console.log(blogs)
     return (
-        <div>
-            <h1>This is blog page</h1>
+        <div className='secondary py-32 w-full'>
+            <div className='text-color text-center'>
+
+                <h1 className='font-bold text-3xl'>
+                    Feeding Hope: Stories & Tips from Food Donation
+                </h1>
+                <p className='text-sm mt-3'>
+                    
+                    Discover inspiring stories, helpful tips, and the latest news to fight hunger and reduce food waste.
+                </p>
+
+            </div>
+
+            <div className='responsive pt-10 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+                {
+                    blogs.map(blog=><Blog key={blog.id} blog={blog}></Blog>)
+                }
+
+            </div>
         </div>
     );
 };
